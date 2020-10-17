155d41ed2ad9137d57a5223d8e6bb76b2b61b6c3
Eliel Coutinho <elielkoutinho@gmail.com>
2020-10-17 01:23:49 -0300

    Ao clicar no link, você verá uma página semelhante àquelamostrada na Figura 18.4

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -8,5 +8,8 @@ from . import views
 urlpatterns = [ 
         # Página inicial
         url(r'^$', views.index, name='index'),
+
+        # Mostra todos os assuntos
+        url(r'^topics/$', views.topics, name='topics'),
         
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 
+from .models import Topic
+
 def index(request):
     """A página inicial de Learning Log"""
     return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """Mostra todos os assuntos."""
+    topics = Topic.objects.order_by('date_added')                       #w
+    context = {'topics': topics}                                        #X
+    return render(request, 'learning_logs/topics.html', context)        #Y 
+
+
     
 
