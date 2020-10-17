8e5667ff1991efb28e4eb3fc03d93a1e9c0a798b
Eliel Coutinho <elielkoutinho@gmail.com>
2020-10-17 03:16:25 -0300

    este a página new_entry adicionando algumas entradas para os assuntosque você criou.

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -2,7 +2,6 @@
 
 from django.conf.urls import url
 
-
 from . import views
 
 urlpatterns = [ 
@@ -14,5 +13,13 @@ urlpatterns = [
 
         # Página de detalhes para um único assunto
         url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
+
+        # Página para adicionar um novo assunto
+        url(r'^new_topic/$', views.new_topic, name='new_topic'),
+
+        # Página para adicionar uma nova entrada
+        url(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry,name='new_entry'),
+
+
         
 ]
