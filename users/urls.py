387463a8dd1a83e530b588c3d4c0deee01d427a3
Eliel Coutinho <elielkoutinho@gmail.com>
2020-10-19 02:34:31 -0300

    Na próxima seção vamos restringir algumas das páginas para que elasestejam disponíveis somente aos usuários cadastrados e garantir que todosos assuntos pertençam a um usuário específico.

--- users/urls.py
+++ users/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
     #Página de login
     url(r'^login/$', login, {'template_name': 'users/login.html'}, name='login'),
     url(r'^logout/$', views.logout_view, name='logout'),
+
+    #Página de cadastro
+    url(r'^register/$', views.register, name='register'),
     
 ]
